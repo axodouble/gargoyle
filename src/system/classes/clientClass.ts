b2648c56ce6ec4a583ab9403c69107b0cdda20be
Axodouble <git@axodouble.com>
2024-10-12 21:47:48 +0200

    Refactor event registration logic

--- src/system/classes/clientClass.ts
+++ src/system/classes/clientClass.ts
@@ -1,27 +1,39 @@
-import { Client, ClientOptions } from 'discord.js';
-import Database from '@database/database.js';
+import { Client, ClientOptions, Collection } from 'discord.js';
+// import Database from '@database/database.js';
 import { debug, error, log, warning } from '../tools/logger.js';
-import loadEvents from '../initializers/loadEvents.js';
+import registerEvents from '../initializers/registerEvents.js';
+import GargoyleCommand from './commandClass.js';
 
 class GargoyleClient extends Client {
-    public db: Database;
+    // db: Database;
+    commands: Collection<string, GargoyleCommand>;
+
     constructor(options: ClientOptions) {
         super(options);
-        this.db = new Database(this);
-
-        loadEvents(this);
+        // this.db = new Database(this);
+        this.commands = new Collection();
+        this.loadEvents();
     }
+
     public log(message: string) {
         log(message);
     }
+
     public debug(message: string) {
         debug(message);
     }
+
     public error(message: string) {
         error(message);
     }
+
     public warning(message: string) {
         warning(message);
+    }
+
+    public async loadEvents() {
+        this.log('Loading events...');
+        await registerEvents(this, '../../events');
     }
 
     override login(token?: string) {
---
+++ src/system/initializers/registerEvents.ts
@@ -0,0 +1,35 @@
+import fs from 'fs/promises';
+import path from 'path';
+import GargoyleClient from '../classes/clientClass.js';
+
+async function registerEvents(client: GargoyleClient, ...dirs: string[]): Promise<void> {
+    client.debug('Beginning event registration...');
+    for (const dir of dirs) {
+        const files = await fs.readdir(path.join(__dirname, dir));
+        client.debug(`Loading events from ${dir}...`);
+
+        for (const file of files) {
+            client.debug(`Loading file: ${file}`);
+            const stat = await fs.lstat(path.join(__dirname, dir, file));
+
+            if (file.includes('-ignore')) continue;
+
+            if (stat.isDirectory()) {
+                client.debug(`Loading events from ${file}...`);
+                await registerEvents(client, path.join(dir, file));
+            }
+            else if (file.endsWith('.js')) {
+                const eventName = file.substring(0, file.indexOf('.js'));
+                try {
+                    const eventModule = (await import(path.join(__dirname, dir, file))).default;
+                    client.debug(`Registering event: ${eventName}`);
+                    client.on(eventName, eventModule.bind(null, client));
+                } catch(e) {
+                    client.error(e as string);
+                }
+            }
+        }
+    }
+}
+
+export default registerEvents;
