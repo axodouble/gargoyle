7c0697f0e5f9ec7e8803374afa325355d88a1dff
Axodouble <git@axodouble.com>
2024-10-14 20:15:58 +0200

    Updated database imports

--- src/system/database/database.ts
+++ src/system/database/database.ts
@@ -1,25 +1,25 @@
 import mongoose from 'mongoose';
 import { databaseGuilds, getGuild } from '@src/system/database/models/databaseGuildSchema.js';
-import GargoyleClient from '@src/system/classes/clientClass.js';
+import GargoyleClient from '@src/system/classes/gargoyleClient.js';
 import { databaseGuildUsers, getGuildUser } from '@src/system/database/models/databaseGuildUserSchema.js';
 
 class Database extends mongoose.Connection {
-    constructor(gargoyleClient: GargoyleClient) {
-        gargoyleClient.log('Connecting to the database');
+    constructor(client: GargoyleClient) {
+        client.logger.log('Connecting to the database');
         super();
         const uri = process.env.MONGO_URI;
         if (!uri) {
-            gargoyleClient.error('No MongoDB URI provided');
+            client.logger.error('No MongoDB URI provided');
             process.exit(1);
         }
 
         mongoose
             .connect(uri)
             .then(() => {
-                gargoyleClient.log('Connected to the database');
+                client.logger.log('Connected to the database');
             })
             .catch((err) => {
-                gargoyleClient.error(err);
+                client.logger.error(err);
             });
     }
     public databaseGuilds = databaseGuilds;
