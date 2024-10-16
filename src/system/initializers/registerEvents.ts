135ea2e661734e3c14d7875c311390f7cb065209
Axodouble <git@axodouble.com>
2024-10-16 20:41:33 +0200

    Removed useless tracing in registerEvents & added catch for failed registering on event.

--- src/system/initializers/registerEvents.ts
+++ src/system/initializers/registerEvents.ts
@@ -3,8 +3,7 @@ import path from 'path';
 import GargoyleClient from '../classes/gargoyleClient.js';
 import GargoyleEvent from '../classes/gargoyleEvent.js';
 
-async function registerEvents(client: GargoyleClient, ...dirs: string[]) {
-    client.logger.trace('Running registerEvents...');
+async function registerEvents(client: GargoyleClient, ...dirs: string[]): Promise<void> {
     for (const dir of dirs) {
         const files = await fs.readdir(path.join(__dirname, dir));
 
@@ -12,17 +11,20 @@ async function registerEvents(client: GargoyleClient, ...dirs: string[]) {
             const stat = await fs.lstat(path.join(__dirname, dir, file));
 
             if (stat.isDirectory()) {
-                client.logger.trace(`Loading events from ${file}...`);
                 await registerEvents(client, path.join(dir, file));
             } else if (file.endsWith('.ts') || file.endsWith('.js')) {
-                const { default: Event } = await import(path.join(__dirname, dir, file));
-                const event: GargoyleEvent = new Event();
-                if (event.once) {
-                    client.logger.trace(`Registering event as once: ${event.event}`);
-                    client.once(event.event, (...args) => event.execute(client, ...args));
-                } else {
-                    client.logger.trace(`Registering event as on: ${event.event}`);
-                    client.on(event.event, (...args) => event.execute(client, ...args));
+                try {
+                    const { default: Event } = await import(path.join(__dirname, dir, file));
+                    const event: GargoyleEvent = new Event();
+                    if (event.once) {
+                        client.logger.trace(`Registering event as once: ${event.event}`);
+                        client.once(event.event, (...args) => event.execute(client, ...args));
+                    } else {
+                        client.logger.trace(`Registering event as on: ${event.event}`);
+                        client.on(event.event, (...args) => event.execute(client, ...args));
+                    }
+                } catch (err) {
+                    client.logger.error(err as string, `Error registering event: ${file}`);
                 }
             }
         }
