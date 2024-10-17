be3735e5b6a260d6696408244bb1238cda68ce0b
Axodouble <git@axodouble.com>
2024-10-17 20:07:58 +0200

    Changed logging of event registration to be the filename.

--- src/system/initializers/registerEvents.ts
+++ src/system/initializers/registerEvents.ts
@@ -17,10 +17,10 @@ async function registerEvents(client: GargoyleClient, ...dirs: string[]): Promis
                     const { default: Event } = await import(path.join(__dirname, dir, file));
                     const event: GargoyleEvent = new Event();
                     if (event.once) {
-                        client.logger.trace(`Registering event as once: ${event.event}`);
+                        client.logger.trace(`Registering event as once: ${file}`);
                         client.once(event.event, (...args) => event.execute(client, ...args));
                     } else {
-                        client.logger.trace(`Registering event as on: ${event.event}`);
+                        client.logger.trace(`Registering event as on: ${file}`);
                         client.on(event.event, (...args) => event.execute(client, ...args));
                     }
                 } catch (err) {
