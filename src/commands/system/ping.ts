df36d18ad26041757142eb47fe88a33474fc1ea0
Axodouble <git@axodouble.com>
2024-10-17 07:19:06 +0000

    Added underscore for unused declarations

--- src/commands/system/ping.ts
+++ src/commands/system/ping.ts
@@ -8,11 +8,11 @@ export default class Ping extends GargoyleCommand {
     public override slashCommand = new SlashCommandBuilder().setName('ping').setDescription('Replies with Pong!');
     public override textCommand = new TextCommandBuilder().setName('ping').setDescription('Replies with Pong!').addAlias('p');
 
-    public override executeSlashCommand(client: GargoyleClient, interaction: ChatInputCommandInteraction) {
+    public override executeSlashCommand(_client: GargoyleClient, interaction: ChatInputCommandInteraction) {
         interaction.reply('Pong!');
     }
 
-    public override executeTextCommand(client: GargoyleClient, message: Message) {
+    public override executeTextCommand(_client: GargoyleClient, message: Message) {
         message.reply('Pong!');
     }
 }
