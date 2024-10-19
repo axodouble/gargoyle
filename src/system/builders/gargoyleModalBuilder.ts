7d6538c6208a6a2f3eb91a751d965528a7fe61c0
Axodouble <git@axodouble.com>
2024-10-19 13:49:56 +0200

    Wrote modalbuilder with documentation

---
+++ src/system/builders/gargoyleModalBuilder.ts
@@ -0,0 +1,25 @@
+import { ModalBuilder } from 'discord.js';
+import GargoyleCommand from '../classes/gargoyleCommand.js';
+
+/**
+ * A builder class for creating custom modals specific to Gargoyle commands.
+ * Extends the `ModalBuilder` class.
+ *
+ * @class
+ * @extends {ModalBuilder}
+ */
+class GargoyleModalBuilder extends ModalBuilder {
+    /**
+     * Creates an instance of GargoyleModalBuilder.
+     *
+     * @constructor
+     * @param {GargoyleCommand} command - The command associated with the modal.
+     * @param {string} argument - The argument to be used and referenced for execution in the command.
+     */
+    constructor(command: GargoyleCommand, argument: string) {
+        super();
+        this.setCustomId(`cmd-${command.slashCommand?.name.toLowerCase() ?? command.textCommand?.name.toLowerCase()}-${argument.toLowerCase()}`);
+    }
+}
+
+export default GargoyleModalBuilder;
