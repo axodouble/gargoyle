4748882f9f959a6748977a2587dc6b119a04edd5
Axodouble <git@axodouble.com>
2024-12-30 19:48:39 +0800

    Fixed formatting

--- src/events/entropy/rolePrefixes.ts
+++ src/events/entropy/rolePrefixes.ts
@@ -1,23 +1,23 @@
 import GargoyleClient from '@src/system/backend/classes/gargoyleClient.js';
 import GargoyleEvent from '@src/system/backend/classes/gargoyleEvent.js';
-import { Events, GuildMember, Status } from 'discord.js';
+import { Events, GuildMember } from 'discord.js';
 
 export default class Ready extends GargoyleEvent {
     public event = Events.GuildMemberUpdate as const;
 
-    public async execute(client: GargoyleClient, member: GuildMember): Promise<void> {
+    public async execute(_client: GargoyleClient, member: GuildMember): Promise<void> {
         if (member.guild.id !== '1039152052644880435') return;
 
-        let updatedMember = await member.fetch(true)
-        let namePrefix = "["
+        const updatedMember = await member.fetch(true);
+        let namePrefix = '[';
 
-        updatedMember.roles.cache.forEach(role => {
-            if (role.name === "@everyone") return;
-            namePrefix += role.name.split("")[0].toUpperCase()
-        })
+        updatedMember.roles.cache.forEach((role) => {
+            if (role.name === '@everyone') return;
+            namePrefix += role.name.split('')[0].toUpperCase();
+        });
 
-        namePrefix += "] " + updatedMember.user.displayName
+        namePrefix += `] ${updatedMember.user.displayName}`;
 
-        updatedMember.setNickname(namePrefix).catch(err => { })
+        updatedMember.setNickname(namePrefix).catch(() => {});
     }
 }
