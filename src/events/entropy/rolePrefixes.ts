f51d63e9ea143e82fe0471201a67c07445b72f63
Axodouble <git@axodouble.com>
2024-12-30 19:44:10 +0800

    Add role prefix update on GuildMemberUpdate event for Entropy

---
+++ src/events/entropy/rolePrefixes.ts
@@ -0,0 +1,23 @@
+import GargoyleClient from '@src/system/backend/classes/gargoyleClient.js';
+import GargoyleEvent from '@src/system/backend/classes/gargoyleEvent.js';
+import { Events, GuildMember, Status } from 'discord.js';
+
+export default class Ready extends GargoyleEvent {
+    public event = Events.GuildMemberUpdate as const;
+
+    public async execute(client: GargoyleClient, member: GuildMember): Promise<void> {
+        if (member.guild.id !== '1039152052644880435') return;
+
+        let updatedMember = await member.fetch(true)
+        let namePrefix = "["
+
+        updatedMember.roles.cache.forEach(role => {
+            if (role.name === "@everyone") return;
+            namePrefix += role.name.split("")[0].toUpperCase()
+        })
+
+        namePrefix += "] " + updatedMember.user.displayName
+
+        updatedMember.setNickname(namePrefix).catch(err => { })
+    }
+}
