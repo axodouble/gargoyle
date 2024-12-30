7996320b6010dbd5995974d0ff6e088a736aec62
Axodouble <git@axodouble.com>
2024-12-30 16:21:27 +0800

    Improve logging of Discord WS status by using status name

--- src/events/basic/blank.ts
+++ src/events/basic/blank.ts
@@ -1,12 +1,14 @@
 import GargoyleClient from '@src/system/backend/classes/gargoyleClient.js';
 import GargoyleEvent from '@src/system/backend/classes/gargoyleEvent.js';
+import { Status } from 'discord.js';
 
 export default class Ready extends GargoyleEvent {
     public event = 'ready' as const;
     override once = true;
 
     public execute(client: GargoyleClient): void {
-        client.logger.trace(`Discord WS Status is ${client.ws.status}`);
+        const statusName = Status[client.ws.status];
+        client.logger.trace(`Discord WS Status is ${statusName}`);
         // Whatever you want to do when the bot is ready goes here.
     }
 }
