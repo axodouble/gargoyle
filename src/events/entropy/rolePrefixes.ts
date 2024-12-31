1e8d180e7095f27979690a0f855e3c7a17be2f06
Axodouble <git@axodouble.com>
2024-12-31 13:03:50 +0800

    Update GuildMemberUpdate event to change guild ID and improve nickname formatting

--- src/events/entropy/rolePrefixes.ts
+++ src/events/entropy/rolePrefixes.ts
@@ -5,8 +5,10 @@ import { Events, GuildMember } from 'discord.js';
 export default class Ready extends GargoyleEvent {
     public event = Events.GuildMemberUpdate as const;
 
-    public async execute(_client: GargoyleClient, member: GuildMember): Promise<void> {
-        if (member.guild.id !== '1039152052644880435') return;
+    public async execute(client: GargoyleClient, member: GuildMember): Promise<void> {
+        if (member.guild.id !== '1009048008857493624') return;
+
+        client.logger.debug(`Updating nickname for ${member.user.tag}`);
 
         const updatedMember = await member.fetch(true);
         let namePrefix = '[';
@@ -16,7 +18,7 @@ export default class Ready extends GargoyleEvent {
             namePrefix += role.name.split('')[0].toUpperCase();
         });
 
-        namePrefix += `] ${updatedMember.user.displayName}`;
+        namePrefix += `] ${updatedMember.nickname?.split(' ').slice(1).join(' ') || updatedMember.user.username}`;
 
         updatedMember.setNickname(namePrefix).catch(() => {});
     }
