88470c36a03dfd82b19ea6032b68ee0861db6432
Axodouble <git@axodouble.com>
2025-01-04 23:03:21 +0800

    Enhance context command handler to support multiple interaction types and improve error handling for command execution

--- src/system/backend/events/commands/handlers/contextCommandHandler.ts
+++ src/system/backend/events/commands/handlers/contextCommandHandler.ts
@@ -1,24 +1,27 @@
 import GargoyleClient from '@src/system/backend/classes/gargoyleClient.js';
 import GargoyleEvent from '@src/system/backend/classes/gargoyleEvent.js';
-import { ContextMenuCommandInteraction } from 'discord.js';
+import { ContextMenuCommandInteraction, MessageContextMenuCommandInteraction, UserContextMenuCommandInteraction } from 'discord.js';
 
 export default class ContextCommandHandler extends GargoyleEvent {
     public event = 'interactionCreate' as const;
 
-    public execute(client: GargoyleClient, interaction: ContextMenuCommandInteraction): void {
-        if (!interaction.isContextMenuCommand) return;
+    public execute(client: GargoyleClient, interaction: ContextMenuCommandInteraction | UserContextMenuCommandInteraction | MessageContextMenuCommandInteraction): void {
+        if (!interaction.isContextMenuCommand()) return;
         if (interaction.user.bot) return;
 
-        const command = client.commands.find((command) => {
-            return command.contextCommands?.find((contextCommand) => contextCommand.name === interaction.command?.name);
-        });
+        const command = client.commands.find((command) =>
+            command.contextCommands?.find((contextCommand) => contextCommand.name === interaction.commandName)
+        );
 
         if (!command) {
+            client.logger.error(`Context command not found: ${interaction.commandName}`);
             interaction.reply('Context command not found!').then((msg) => {
                 setTimeout(() => {
                     msg.delete();
                 }, 5000);
-            });
+            }).catch(() => {
+                client.logger.error('Failed to send context command not found message.');
+            })
         } else {
             command.executeContextMenuCommand(client, interaction);
             return client.logger.trace(`${interaction.user} used the ${interaction.commandName} context command.`);
