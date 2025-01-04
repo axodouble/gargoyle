87cb90c4ee7b16ef9106eede50fcfaf498bb8eb9
Axodouble <git@axodouble.com>
2025-01-04 23:03:10 +0800

    Refactor sendAsServer to return a Promise and implement editAsServer for editing messages via webhooks

--- src/system/backend/tools/server.ts
+++ src/system/backend/tools/server.ts
@@ -1,7 +1,7 @@
-import { MessageCreateOptions, TextChannel } from 'discord.js';
+import { MessageCreateOptions, MessageResolvable, TextChannel } from 'discord.js';
 
-export function sendAsServer(message: MessageCreateOptions, channel: TextChannel): void {
-    channel.fetchWebhooks().then(async (webhooks) => {
+export function sendAsServer(message: MessageCreateOptions, channel: TextChannel): Promise<void> {
+    return channel.fetchWebhooks().then(async (webhooks) => {
         let webhook;
 
         if (webhooks.size === 0) {
@@ -18,5 +18,37 @@ export function sendAsServer(message: MessageCreateOptions, channel: TextChannel
             username: channel.guild?.name || channel.client.user.username,
             ...message
         });
+    }).catch(() => {
+        throw new Error('Failed to send message as server.');
+    });
+}
+
+export function editAsServer(message: MessageCreateOptions, channel: TextChannel, messageId: string | MessageResolvable): Promise<boolean> {
+    return channel.fetchWebhooks().then(async (webhooks) => {
+        let webhook;
+
+        if (webhooks.size === 0) {
+            webhook = await channel.createWebhook({
+                name: channel.guild?.name || channel.client.user.username,
+                reason: 'Server Message'
+            });
+        } else {
+            webhook = webhooks.first();
+        }
+
+        let messageEdit: MessageResolvable;
+
+        if (typeof messageId === 'string') {
+            messageEdit = await channel.messages.fetch(messageId);
+        } else {
+            messageEdit = messageId;
+        }
+
+        await webhook?.editMessage(messageEdit, {
+            ...message,
+        });
+        return true;
+    }).catch(() => {
+        return false;
     });
 }
