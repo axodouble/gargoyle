436de669ef996f6710df032e22f27f780dcade94
Axodouble <git@axodouble.com>
2025-01-04 23:16:55 +0800

    Refactor sendAsServer and editAsServer functions for improved readability and error handling

--- src/system/backend/tools/server.ts
+++ src/system/backend/tools/server.ts
@@ -1,54 +1,58 @@
 import { MessageCreateOptions, MessageResolvable, TextChannel } from 'discord.js';
 
 export function sendAsServer(message: MessageCreateOptions, channel: TextChannel): Promise<void> {
-    return channel.fetchWebhooks().then(async (webhooks) => {
-        let webhook;
+    return channel
+        .fetchWebhooks()
+        .then(async (webhooks) => {
+            let webhook;
 
-        if (webhooks.size === 0) {
-            webhook = await channel.createWebhook({
-                name: channel.guild?.name || channel.client.user.username,
-                reason: 'Server Message'
+            if (webhooks.size === 0) {
+                webhook = await channel.createWebhook({
+                    name: channel.guild?.name || channel.client.user.username,
+                    reason: 'Server Message'
+                });
+            } else {
+                webhook = webhooks.first();
+            }
+
+            await webhook?.send({
+                avatarURL: channel.guild?.iconURL() || undefined,
+                username: channel.guild?.name || channel.client.user.username,
+                ...message
             });
-        } else {
-            webhook = webhooks.first();
-        }
-
-        await webhook?.send({
-            avatarURL: channel.guild?.iconURL() || undefined,
-            username: channel.guild?.name || channel.client.user.username,
-            ...message
+        })
+        .catch(() => {
+            throw new Error('Failed to send message as server.');
         });
-    }).catch(() => {
-        throw new Error('Failed to send message as server.');
-    });
 }
 
 export function editAsServer(message: MessageCreateOptions, channel: TextChannel, messageId: string | MessageResolvable): Promise<boolean> {
-    return channel.fetchWebhooks().then(async (webhooks) => {
-        let webhook;
+    return channel
+        .fetchWebhooks()
+        .then(async (webhooks) => {
+            let webhook;
 
-        if (webhooks.size === 0) {
-            webhook = await channel.createWebhook({
-                name: channel.guild?.name || channel.client.user.username,
-                reason: 'Server Message'
-            });
-        } else {
-            webhook = webhooks.first();
-        }
+            if (webhooks.size === 0) {
+                webhook = await channel.createWebhook({
+                    name: channel.guild?.name || channel.client.user.username,
+                    reason: 'Server Message'
+                });
+            } else {
+                webhook = webhooks.first();
+            }
 
-        let messageEdit: MessageResolvable;
+            let messageEdit: MessageResolvable;
 
-        if (typeof messageId === 'string') {
-            messageEdit = await channel.messages.fetch(messageId);
-        } else {
-            messageEdit = messageId;
-        }
+            if (typeof messageId === 'string') {
+                messageEdit = await channel.messages.fetch(messageId);
+            } else {
+                messageEdit = messageId;
+            }
 
-        await webhook?.editMessage(messageEdit, {
-            ...message,
+            await webhook?.editMessage(messageEdit, { ...message });
+            return true;
+        })
+        .catch(() => {
+            return false;
         });
-        return true;
-    }).catch(() => {
-        return false;
-    });
 }
