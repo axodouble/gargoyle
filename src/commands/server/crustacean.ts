0ef5475ead23d8abe15d666652639b0948c91707
Axodouble <git@axodouble.com>
2025-03-24 10:40:33 +0000

    Added crustacean enable functionality and database user and guild entries

--- src/commands/server/crustacean.ts
+++ src/commands/server/crustacean.ts
@@ -13,12 +13,18 @@ export default class Server extends GargoyleCommand {
             .setDescription('Crustacean invite system')
             .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
             .addSubcommand((subcommand) => subcommand.setName('info').setDescription('Get information about the crustacean system'))
+            .addSubcommand((subcommand) =>
+                subcommand
+                    .setName('enable')
+                    .setDescription('Enable or disable the crustacean system')
+                    .addBooleanOption((option) => option.setName('enable').setDescription('Enable or disable the system').setRequired(true))
+            )
             .setContexts([InteractionContextType.Guild]) as GargoyleSlashCommandBuilder
     ];
 
     public override async executeSlashCommand(_client: GargoyleClient, interaction: ChatInputCommandInteraction) {
         if (interaction.options.getSubcommand() === 'info') {
-            interaction.reply({
+            return interaction.reply({
                 embeds: [
                     new GargoyleEmbedBuilder()
                         .setTitle('Crustacean Invite System')
@@ -35,6 +41,27 @@ export default class Server extends GargoyleCommand {
                 ],
                 flags: MessageFlags.Ephemeral
             });
+        } else if (interaction.options.getSubcommand() === 'enable') {
+            const guildId = interaction.guildId;
+            if (!guildId) return interaction.reply({ content: `This command can only be used in a guild`, flags: MessageFlags.Ephemeral });
+            let guild = await getCrustaceanGuild(guildId);
+
+            if (guild.enabled == interaction.options.getBoolean('enable', true)) {
+                return interaction.reply({
+                    content: `Crustacean system is already ${guild.enabled ? 'enabled' : 'disabled'}`,
+                    flags: MessageFlags.Ephemeral
+                });
+            }
+
+            guild.enabled = !guild.enabled;
+            await guild.save();
+
+            return interaction.reply({
+                content: `Crustacean system has been ${guild.enabled ? 'enabled' : 'disabled'}`,
+                flags: MessageFlags.Ephemeral
+            });
+        } else {
+            return interaction.reply({ content: `Not implemented yet, sorry.`, flags: MessageFlags.Ephemeral });
         }
     }
 
@@ -50,4 +77,69 @@ class MemberJoin extends GargoyleEvent {
         //     const invite = invites.find((invite) => invite.uses! < invite.maxUses!);
         //     if (invite) {
     }
+}
+
+import { Schema, model } from 'mongoose';
+
+const crustaceanGuildSchema = new Schema({
+    guildId: String,
+    enabled: {
+        type: Boolean,
+        default: false
+    },
+    channel: {
+        type: String,
+        default: null
+    }
+});
+
+const crustaceanUserSchema = new Schema({
+    userId: String,
+    guildId: String, // Users are unique per guild
+    inviterId: {
+        type: String, // The user who "invited" the user to the guild, or accepted the joinee
+        default: null // this can be null to account for users who are not invited yet or have no logged invitee.
+    },
+    reputation: {
+        value: {
+            type: Number,
+            default: 0
+        },
+        log: [
+            {
+                userId: String, // User who caused the reputation value increase
+                value: Number, // The value it would have increased by
+                reason: String, // Reason for the increase, admin or user
+                timestamp: Date // When the increase happened
+            }
+        ]
+    }
+});
+
+const databaseCrustaceanGuild = model('CrustaceanGuilds', crustaceanGuildSchema);
+const databaseCrustaceanUser = model('CrustaceanUsers', crustaceanUserSchema);
+
+async function getCrustaceanGuild(guildId: string) {
+    let crustaceanGuild = await databaseCrustaceanGuild.findOne({ guildId: guildId });
+    if (!crustaceanGuild) {
+        crustaceanGuild = new databaseCrustaceanGuild({
+            guildId: guildId
+        });
+        await crustaceanGuild.save();
+    }
+
+    return crustaceanGuild;
+}
+
+async function getCrustaceanUser(userId: string, guildId: string) {
+    let crustaceanUser = await databaseCrustaceanUser.findOne({ userId: userId, guildId: guildId });
+    if (!crustaceanUser) {
+        crustaceanUser = new databaseCrustaceanUser({
+            userId: userId,
+            guildId: guildId
+        });
+        await crustaceanUser.save();
+    }
+
+    return crustaceanUser;
 }
