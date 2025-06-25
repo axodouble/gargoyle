906bf5d0a65e65bfe43ef7789240d18925a1782c
Axodouble <git@axodouble.com>
2025-06-25 08:14:01 +0800

    Fixed user finding in whois

--- src/system/commands/manage.ts
+++ src/system/commands/manage.ts
@@ -13,14 +13,12 @@ export default class Manage extends GargoyleCommand {
     public override async executeTextCommand(client: GargoyleClient, message: Message, ...args: string[]): Promise<void> {
         if (message.author.id !== '244173330431737866') return;
         if (args[0] === 'whois') {
-            const user = message.mentions.users.first() || message.author;
-            const member = message.guild?.members.cache.get(args[1] || user.id);
-            if (member) {
-                const roles = member.roles.cache.map((role) => role.name).join(', ');
-                message.reply(`User: ${user.tag}\nID: ${user.id}\nRoles: ${roles}\nCommand: ${args.join(' ')}`);
-            } else {
-                message.reply(`User: ${user.tag}\nID: ${user.id}\nRoles: None (not in this guild)\nCommand: ${args.join(' ')}`);
-            }
+            const user = await client.users.fetch(args[1]).catch(async (err) => {
+                await message.reply(`Error trying to find user: \`${err.message}\``);
+                return;
+            });
+
+            if (user) await message.reply(`User: ${user.tag}\nID: ${user.id}`);
         } else if (args[0] === 'manage' || args[0] === 'mgmt') {
             if (args.length > 1) {
                 if (args[1] === 'guilds') {
