3493e2494780268873004165578fe6a6a2938845
Axodouble <git@axodouble.com>
2025-07-29 13:30:27 +0800

    Removed logging for command not found in API request & added early return for API pathnames

--- src/system/backend/tools/web.ts
+++ src/system/backend/tools/web.ts
@@ -4,6 +4,10 @@ export default async function executeWebRequest(client: GargoyleClient, request:
     const url = new URL(request.url);
     const commandName = url.pathname.split('/')[2];
 
+    if (!commandName) {
+        return new Response('Not Found', { status: 404, headers: { 'Content-Type': 'text/plain' } });
+    }
+
     const command = client.commands.find((command) => {
         return (
             command.slashCommand?.name === commandName ||
@@ -14,7 +18,6 @@ export default async function executeWebRequest(client: GargoyleClient, request:
     });
 
     if (!command) {
-        client.logger.error(`Command ${commandName} not found.`);
         return new Response('Not Found', { status: 404, headers: { 'Content-Type': 'text/plain' } });
     } else {
         client.logger.trace(`Executing command ${commandName} from web request.`);
