76fd660cbdeb6b99779580b6d35a522a54b25665
Axodouble <git@axodouble.com>
2025-10-18 16:43:54 +0800

    Fixed code to let daily be claimed based purely on a different day other than timespan since

--- src/modules/fun/economy.ts
+++ src/modules/fun/economy.ts
@@ -67,7 +67,7 @@ export default class Economy extends GargoyleModule {
                 const lastDaily = new Date(economyUser.lastDaily);
                 const diffTime = Math.abs(now.getTime() - lastDaily.getTime());
                 const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-                if (diffDays === 0) {
+                if (lastDaily.getDate() === now.getDate() && lastDaily.getMonth() === now.getMonth() && lastDaily.getFullYear() === now.getFullYear()) {
                     await interaction.reply({
                         components: [new GargoyleContainerBuilder('You have already claimed your daily reward today!')],
                         flags: [MessageFlags.Ephemeral, MessageFlags.IsComponentsV2]
