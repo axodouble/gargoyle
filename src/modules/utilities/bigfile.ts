c29c6c2e8ed60bfeb9c26cc414389c2e90ab0b38
Axodouble <git@axodouble.com>
2025-10-27 16:25:09 +0800

    Added a new command that lets you upload files over the 8MB limit

---
+++ src/modules/utilities/bigfile.ts
@@ -0,0 +1,23 @@
+import GargoyleSlashCommandBuilder from '@src/system/backend/builders/gargoyleSlashCommandBuilder.js';
+import GargoyleClient from '@src/system/backend/classes/gargoyleClient.js';
+import GargoyleModule from '@src/system/backend/classes/gargoyleModule.js';
+import { ApplicationIntegrationType, ChatInputCommandInteraction } from 'discord.js';
+
+export default class BigFile extends GargoyleModule {
+    public override category: string = 'utilities';
+
+    public override slashCommands: GargoyleSlashCommandBuilder[] = [
+        new GargoyleSlashCommandBuilder()
+            .setName('bigfile')
+            .setDescription('Lets you share files over the 8MB Discord limit')
+            .setIntegrationTypes(ApplicationIntegrationType.GuildInstall, ApplicationIntegrationType.UserInstall)
+
+            .addAttachmentOption((option) =>
+                option.setName('file').setDescription('The file you want to share').setRequired(true)
+            ) as GargoyleSlashCommandBuilder
+    ];
+
+    public override executeSlashCommand(_client: GargoyleClient, interaction: ChatInputCommandInteraction): void {
+        interaction.reply(interaction.options.getAttachment('file', true).url);
+    }
+}
