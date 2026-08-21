ff855c90dc2c6d6402843d8eb8a25a92d05ea10d
Jasper (Axodouble) V. <software@jas.pe>
2026-08-21 13:48:22 +0000

    feat: add factions module skeleton with db helpers

---
+++ src/modules/factions/factions.ts
@@ -0,0 +1,6 @@
+import GargoyleModule from '@src/system/backend/classes/gargoyleModule.js';
+
+export default class Factions extends GargoyleModule {
+    public override name: string = 'factions';
+    public override category: string = 'factions';
+}
